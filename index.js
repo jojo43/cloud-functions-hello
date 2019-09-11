a655e4967e956b51aadbf7a75934840457027bc3
jojo43 <s.jmr0124@gmail.com>
2019-09-11 20:56:32 +0900

    Fix typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  const message = req.query.message || res.body.message || 'Hello world!'
+  const message = req.query.message || req.body.message || 'Hello world!'
   res.status(200).send(message)
 }
